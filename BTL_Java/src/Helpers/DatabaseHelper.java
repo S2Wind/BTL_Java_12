0d4cd8e5bc4e20d3f3db724232413d4bc4f93140
S2Wind <phandacson213@gmail.com>
2021-08-08 14:11:04 +0700

    firstinit

---
+++ BTL_Java/src/Helpers/DatabaseHelper.java
@@ -0,0 +1,31 @@
+
+package my.com.qlsv.helpers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DatabaseHelper {
+    static Connection con;
+    public static Connection openConnection() throws Exception
+    {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String connectionUrl = "jdbc:sqlserver://localhost:1433;database=QLSV;";
+        String dbusername = "sa";
+        String password = "213213";
+        con = DriverManager.getConnection(connectionUrl, dbusername, password);
+        return con;
+    }
+    
+    public static Statement getStatement() throws SQLException
+    {
+        return con.createStatement();
+    }
+    public static PreparedStatement getPrepareStatement(String sql) throws SQLException
+    {
+        return con.prepareStatement(sql);
+    }
+}
